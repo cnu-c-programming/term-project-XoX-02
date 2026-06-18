dc43a24b87a06cbdcac18829bb9894de7400fed3
XoX-02 <boy3898@naver.com>
2026-06-18 14:32:23 +0900

    command update

--- command.c
+++ command.c
@@ -1,6 +1,7 @@
 #include "command.h"
 #include "file_io.h"
 #include "student.h"
+#include <stdio.h>
 
 char *p_fname;
 
@@ -24,12 +25,82 @@ ShellResult handle_reload(char* args, Student** head){
         return SHELL_OK;
     }
 }
-ShellResult handle_add(char* args, Student** head);
-ShellResult handle_delete(char* args, Student** head);
-ShellResult handle_update(char* args, Student** head);
-ShellResult handle_find(char* args, Student** head);
-ShellResult handle_list(char* args, Student** head);
-ShellResult handle_stats(char* args, Student** head);
+ShellResult handle_add(char* args, Student** head){
+    int id;
+    char name[32];
+    int score;
+    int arg_num = sscanf(args,"%d %s %d", &id, name, &score);
+
+    if(arg_num < 3){
+        printf("Error: Format incorect\n");
+        return SHELL_ERR_MISSING_ARGUMENT;
+    }
+    if(id <= 0 || (score < 0 || score > 100)){
+        return SHELL_ERR_INVALID_ARGUMENT;
+    }
+    Student *s = find(*head,id);
+    if(s != NULL){
+        printf("Error: Duplicate id\n");
+        return SHELL_ERR_DUPLICATE_STUDENT;
+    }
+    add(head,id,name,score);
+    return SHELL_OK;
+
+}
+ShellResult handle_delete(char* args, Student** head){
+    int id;
+    int arg_num = sscanf(args,"%d",&id);
+    if(arg_num < 1){
+        return SHELL_ERR_MISSING_ARGUMENT;
+    }
+    Student *s = find(*head, id);
+    if(s == NULL){
+        printf("Error: student not found.\n"); 
+        return SHELL_ERR_STUDENT_NOT_FOUND;
+    }
+    delete(head,id);
+    return SHELL_OK;
+}
+ShellResult handle_update(char* args, Student** head){
+    int id;
+    int score;
+    int arg_num = sscanf(args,"%d %d", &id, &score);
+    if(arg_num < 2){
+        return SHELL_ERR_MISSING_ARGUMENT;
+    }
+    if(score < 0 || score > 100){
+        return SHELL_ERR_INVALID_ARGUMENT;
+    }
+    Student *s = find(*head,id);
+    if(s == NULL){
+        return SHELL_ERR_STUDENT_NOT_FOUND;
+    }else {
+        s -> score = score;
+        return SHELL_OK;
+    }
+}
+ShellResult handle_find(char* args, Student** head){
+    int id;
+    int arg_num = sscanf(args,"%d", &id);
+    if(arg_num < 1){
+        return SHELL_ERR_MISSING_ARGUMENT;
+    }
+    Student *s = find(*head,id);
+    if(s == NULL){
+        return SHELL_ERR_STUDENT_NOT_FOUND;
+    }else{
+        printf("ID: %d\nName: %s\nScore: %d\n",s->id,s->name,s->score);
+        return SHELL_OK;
+    }
+}
+ShellResult handle_list(char* args, Student** head){
+    list(*head);
+    return SHELL_OK;
+}
+ShellResult handle_stats(char* args, Student** head){
+    stats(*head);
+    return SHELL_OK;
+}
 ShellResult handle_help(char* args, Student** head);
 ShellResult handle_clear(char* args, Student** head);
 ShellResult handle_exit(char* args, Student** head);
--- student.c
+++ student.c
@@ -2,20 +2,16 @@
 #include "student.h"
 
 void add (Student** head, int id, char *name, int score){
-    /*특히 중복된 ID가 입력된 경우 
-    "duplicate" 또는 "Duplicate"가 포함된 에러 메시지를 출력, 
-    점수 범위를 초과하는 등 잘못된 입력이 들어오면 
-    "Error"를 포함한 메시지를 출력해야 합니다.*/
-    Student *s = malloc(sizeof(Stduent));
+    Student *s = malloc(sizeof(Student));
     if (s == NULL) {
         printf("Error: Fail malloc\n");
-        return;
+        return ;
     }
     s->id = id; strncpy(s->name,name,sizeof(s->name)); s->score = score; s->next = NULL;
     
     if(*head == NULL){
         *head = s;
-        return;
+        return ;
     }else{
         Student *p = *head;
         while(p->next != NULL) { p = p -> next;}
@@ -35,10 +31,7 @@ void update(Student** head, int id, int score){
     
 }
 void delete(Student** head, int id){
-    if (*head == NULL) {
-        printf("Error: student not found.\n"); // 
-        return;
-    }
+    if (*head == NULL) return;
     Student *p = *head;
 
     if(p->id == id){
@@ -50,10 +43,8 @@ void delete(Student** head, int id){
     while(p->next != NULL && p->next->id != id){
          p = p->next;
     }
-   
-    if(p->next == NULL){
-        printf("Error: student not found.\n");
-    }else{
+    
+    if(p->nest != NULL){
         Student *t = p->next->next;
         free(p->next);
         p->next = t;
